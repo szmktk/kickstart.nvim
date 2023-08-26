479ccffa0e8ef9292d5878b1988aad675b2cfca8
Szymon Mikitiuk <szymon.mikitiuk@infermedica.com>
2023-08-27 01:14:41 +0200

    Add LSP configuration

--- lua/ukos/init.lua
+++ lua/ukos/init.lua
@@ -1,5 +1,6 @@
 require("ukos.aucmd")
 require("ukos.remap")
 require("ukos.lazy")
+require("ukos.lsp")
 require("ukos.set")
 
--- lua/ukos/remap.lua
+++ lua/ukos/remap.lua
@@ -44,18 +44,6 @@ vim.keymap.set("n", "<leader>ft", "<cmd>Filetypes<CR>")
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- other remaps
-vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>")
-vim.keymap.set("n", "<leader>tw", "<cmd>set wrap!<CR>")
-vim.keymap.set("n", "<leader>rs", "<cmd>source Session.vim<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
--- try those out once LSP is fully functional ("quick-fix list navigation")
--- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
--- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
--- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
--- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
 -- very useful remaps for clipboard management
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
@@ -64,4 +52,22 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 -- replace the word under cursor in the scope of the current buffer
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+-- try those out once LSP is fully functional ("quick-fix list navigation")
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- other remaps
+vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>")
+vim.keymap.set("n", "<leader>tw", "<cmd>set wrap!<CR>")
+vim.keymap.set("n", "<leader>rs", "<cmd>source Session.vim<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
