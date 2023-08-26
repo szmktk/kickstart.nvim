cfeb4c2f7e38b53a84cc62b442bd644b006f56e8
Szymon Mikitiuk <szymon.mikitiuk@infermedica.com>
2023-08-26 23:03:57 +0200

    Add LSP & telescope plugins

--- lua/ukos/lazy.lua
+++ lua/ukos/lazy.lua
@@ -47,6 +47,73 @@ require('lazy').setup({
   "windwp/nvim-autopairs",
   "kdheepak/lazygit.nvim",
 
-  { "akinsho/toggleterm.nvim", version = "*", config = true }
+  { "akinsho/toggleterm.nvim", version = "*", config = true },
 
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+  },
+  {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+  },
+
+  -- TODO check out these plugins after LSP is configured
+  -- 'tpope/vim-fugitive',
+  -- 'tpope/vim-rhubarb',
+  -- 'tpope/vim-sleuth',
+  -- { 'folke/which-key.nvim', opts = {} },
+  -- { 'numToStr/Comment.nvim', opts = {} },  -- "gc" to comment visual regions/lines
+  -- {
+  --   -- Add indentation guides even on blank lines
+  --   'lukas-reineke/indent-blankline.nvim',
+  --   -- Enable `lukas-reineke/indent-blankline.nvim`
+  --   -- See `:help indent_blankline.txt`
+  --   opts = {
+  --     char = '┊',
+  --     show_trailing_blankline_indent = false,
+  --   },
+  -- },
 })
