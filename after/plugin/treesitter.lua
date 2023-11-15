622714b4efa398c13d9a8737e8de99da0d65e298
Szymon Mikitiuk <singlet.bunches_0m@icloud.com>
2023-11-15 20:11:56 +0100

    Defer Treesitter setup to improve startup time of nvim {filename}

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,62 +1,65 @@
-require("nvim-treesitter.configs").setup {
-  modules = {},
-  sync_install = false,
-  ensure_installed = "all",
-  ignore_install = {},
-  auto_install = true,
-  highlight = { enable = true, additional_vim_regex_highlighting = false },
-  indent = { enable = false },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<C-b>",
-      node_incremental = "<C-b>",
-      scope_incremental = "grc",
-      node_decremental = "<C-s>",
-    },
-  },
-  textobjects = {
-    select = {
+vim.defer_fn(function()
+  require("nvim-treesitter.configs").setup {
+    modules = {},
+    sync_install = false,
+    ensure_installed = "all",
+    ignore_install = {},
+    auto_install = false,
+    highlight = { enable = true, additional_vim_regex_highlighting = false },
+    -- indent = { enable = false },
+    indent = { enable = true },
+    incremental_selection = {
       enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
       keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["aa"] = "@parameter.outer",
-        ["ia"] = "@parameter.inner",
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
+        init_selection = "<C-b>",
+        node_incremental = "<C-b>",
+        scope_incremental = "grc",
+        node_decremental = "<C-s>",
       },
     },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+        },
       },
-      goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]]"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]M"] = "@function.outer",
+          ["]["] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[["] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[M"] = "@function.outer",
+          ["[]"] = "@class.outer",
+        },
       },
-      goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
       },
     },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
-      },
-    },
-  },
-}
+  }
+end, 0)
